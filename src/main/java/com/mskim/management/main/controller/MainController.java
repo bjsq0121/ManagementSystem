7351f2126e52fcd3ba4185e38403c360951490e5
PSJ <PSJ@PSJ-PC>
2018-10-31 16:30:27 +0900

    #. 2018-10-31 김민수. config, properties, mybatis, mariaDB 연동

--- src/main/java/com/mskim/management/main/controller/MainController.java
+++ src/main/java/com/mskim/management/main/controller/MainController.java
@@ -2,20 +2,31 @@ package com.mskim.management.main.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.mskim.management.main.service.MainService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("/main")
 public class MainController {
 	
-	/*재고관리*/
 	
+	@Autowired
+	MainService mainService;
+	
+	//메인페이지
 	@RequestMapping(value = "main.do")
 	public ModelAndView mainPage(HttpServletRequest request){ 
+		log.info("/main/main.do");
 		
+		int userCnt = mainService.test(); //DB컨넥트 테스트용
+		log.info("USER CNT : " + userCnt);
 		
 		ModelAndView mav = new ModelAndView();
 		//mav.addObject("pageParam", pageParam);
